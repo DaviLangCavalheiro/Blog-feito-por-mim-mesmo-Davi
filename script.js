7aa41af242764b1ec2717f9e170daaf171b594f4
Davi Lang Cavalheiro <cavalheiro.davi@escola.pr.gov.br>
2026-07-02 11:48:51 -0300

    mudanças

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Seleciona o botão de alternar tema usando o ID
+const botaoTema = document.getElementById('botao-tema');
 
+// Adiciona um evento de clique ao botão
+botaoTema.addEventListener('click', () => {
+    // Liga/desliga a classe 'modo-escuro' no body da página
+    document.body.classList.toggle('modo-escuro');
+});
